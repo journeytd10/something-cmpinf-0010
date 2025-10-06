fb9c7202ced98439fce7dcdf82b4c9173e03fc73
psa25-source <psa25@pitt.edu>
2025-10-06 13:45:44 -0400

    Updates-Philipa-2.py

--- Untitled-2.py
+++ Untitled-2.py
@@ -9,7 +9,18 @@ def something():
         print("That is amazing! We hope you have a great year!")
     else:
         print("Oh no! Please check out our resources on our website.")
+def branch():
+    school = input("What school do you attend?")
+    if school == "Swanson":
+      print("You are in the Engineering school")
+    elif school == "Dietrich":
+      print("You are in the school for Arts and Sciences")
+
+
+  
+  
 
 if __name__ == "__main__":
     main()
     something()
+    branch()
